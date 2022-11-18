7d32ea87de0b7d6e52467b6cc8dd8f809fe2ff52
Joao.Miziaras <joao.miziara@sptech.school>
2022-11-18 10:06:45 -0300

    novo exe

--- java/ConexaoMySql.java
+++ java/ConexaoMySql.java
@@ -12,7 +12,7 @@ public class ConexaoMySql {
         BasicDataSource datasource = new BasicDataSource();
         datasource.setDriverClassName("com.mysql.cj.jdbc.Driver");
         datasource.setUrl("jdbc:mysql://localhost : 3306/GuardianAngel?useTimezone=true&serverTimezone=UTC");
-        datasource.setUsername("urubu100");
+        datasource.setUsername("ubuntu");
         datasource.setPassword("urubu100");
         conexao = new JdbcTemplate(datasource);
 }
